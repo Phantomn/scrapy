572bbfa50e906cb16a4c51c2b533b22d938b70e1
Phantomn <tmdvyr123@naver.com>
2018-08-04 05:14:13 +0900

    Bithumb 1st Crawl Success

--- bithumb/bithumb/bithumb/pipelines.py
+++ bithumb/bithumb/bithumb/pipelines.py
@@ -4,8 +4,14 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import json
+import codecs
 
 class BithumbPipeline(object):
+    def __init__(self):
+    	self.file = codecs.open("bithumb.json", "wb", encoding='utf-8')
+
     def process_item(self, item, spider):
-        return item
+    	line = json.dumps(dict(item), ensure_ascii=False) + "\n"
+    	self.file.write(line)
+    	return item
--- bithumb/bithumb/bithumb/spiders/bithumb_spider.py
+++ bithumb/bithumb/bithumb/spiders/bithumb_spider.py
@@ -3,6 +3,7 @@ import scrapy
 from scrapy.selector import Selector
 import datetime
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from bithumb.items import BithumbItem
 import mysql.connector
 
@@ -13,33 +14,36 @@ class BithumbSpider(scrapy.Spider):
     start_urls = ['https://bithumb.com/'] #, https://www.bithumb.com/tradeview"]
 
     def __init__(self):
-    	self.browser = webdriver.Firefox()
+        self.options = Options()
+        self.options.add_argument('--headless')
+        self.driver = webdriver.Firefox(firefox_options=self.options, executable_path='/root/scrapy/bithumb/bithumb/geckodriver')
+        print("Firefox Headless Browser Invoked")
 
     def parse(self, response):
-    	self.browser.get(response.url)
-    	html = self.browser.find_element_by_xpath('//*').get_attribute('outerHTML')
-    	self.browser.close()
-    	selector = Selector(text=html)
+        self.driver.get('https://www.bithumb.com')
+        html = self.driver.find_element_by_xpath('//*').get_attribute('outerHTML')
+        self.driver.close()
+        selector = Selector(text=html)
 
-    	for sel in selector.xpath('//table[@id="tableAsset"]/tbody/tr'):
-    		item = BithumbItem()
-    		item['coinName'] = sel.xpath('td[@class="click left_l"]/span[@class="tx_coin"]/text()').extract()[0]
-    		item['coinPrice'] = sel.xpath('td[@class="right click padding_right50 line_td stb"]/strong/text()').extract()[0]
-    		item['date'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
 
-    		self.conn = mysql.connector.connect(
+        for sel in selector.xpath('//table[@id="tableAsset"]/tbody/tr'):
+            item = BithumbItem()
+            item['coinName'] = sel.xpath('td[@class="click left_l"]/span[@class="tx_coin"]/text()').extract()
+            item['coinPrice'] = sel.xpath('td[@class="right click padding_right50 line_td stb"]/strong/text()').extract()
+            item['date'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+
+            '''self.conn = mysql.connector.connect(
     			user = 'root',
     			password = 'tmdvy123',
     			host = '127.0.0.1',
     			database = 'wordpress',
     		)
-    		self.cursor = self.conn.cursor()
+            self.cursor = self.conn.cursor()
 
-    		self.cursor.execute("INSERT INTO cc VALUES(\'%s\', \'%s\', \'%s\')" %(item['coinName'], item['coinPrice'], item['date']))
-    		self.conn.commit()
-    		self.cursor.close()
-    		self.conn.close()
-    		print("================")
-
-    		yield item
-    	print("================")
+            self.cursor.execute("INSERT INTO cc (name, price, date) VALUES(\'%s\', \'%s\', \'%s\')" %(item['coinName'], item['coinPrice'], item['date']))
+            self.conn.commit()
+            self.cursor.close()
+            self.conn.close()
+            print("================")'''
+            yield item
+        print("================")
